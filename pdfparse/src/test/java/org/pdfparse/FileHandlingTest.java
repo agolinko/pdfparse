92960998972e820519b6938ecd2187862ca91d25
Anton Golinko <anton.golinko@gmail.com>
2013-09-14 14:20:07 +0300

    + Added some test files & tests

--- pdfparse/src/test/java/org/pdfparse/FileHandlingTest.java
+++ pdfparse/src/test/java/org/pdfparse/FileHandlingTest.java
@@ -24,7 +24,7 @@ public class FileHandlingTest extends Assert
 
     @Test(expected=org.pdfparse.exception.EParseError.class)
     public void checkMalformedFileParse() throws EParseError, IOException, URISyntaxException {
-        URI uri = this.getClass().getResource("/malformed.pdf").toURI();
+        URI uri = this.getClass().getResource("/malformed_pdfs/noise.pdf").toURI();
         File file = new File(uri);
 
         PDFDocument pp = new PDFDocument(file);
@@ -33,14 +33,41 @@ public class FileHandlingTest extends Assert
 
     @Test
     public void checkSingleFileParse2() throws EParseError, IOException, URISyntaxException {
-        URI uri = this.getClass().getResource("/testfiles/072611harwood.pdf").toURI();
+        URI uri = this.getClass().getResource("/testfiles3/source11.pdf").toURI();
         File file = new File(uri);
 
         PDFDocument pp = new PDFDocument(file);
         pp.dbgDump();
     }
 
-    //@Test
+    @Test
+    public void checkEvilPDFs() throws IOException, URISyntaxException {
+        PDFDocument pp;
+
+        URI uri = this.getClass().getResource("/malformed_pdfs/").toURI();
+        File dir = new File(uri);
+
+        FilenameFilter mask = new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.endsWith( ".pdf" );
+            }
+        };
+
+        for (String filename : dir.list(mask)) {
+            System.out.printf("---- Parsing file: %s ... ", filename);
+
+            try {
+                pp = new PDFDocument(dir.getAbsolutePath() + "\\" + filename);
+                pp.dbgDump();
+                Assert.fail();
+            } catch (EParseError e) {
+                System.out.printf(" %s \r\n", e.getMessage());
+            }
+        }
+    }
+
+    @Test
     public void checkMassiveOpeningForCrash() throws IOException, URISyntaxException {
         PDFDocument pp;
 
